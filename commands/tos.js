517652ccfe9cb3fbe252da3e1ec04bdcc0c4c239
182exe <81325987+182exe@users.noreply.github.com>
2023-07-13 22:07:55 -0700

    improved version checker + tos command
    added version comparison using semver so as not to display warnings when package.json version is updated
    
    tos now gets its text from config

--- commands/tos.js
+++ commands/tos.js
@@ -13,40 +13,19 @@ module.exports = {
             const fileData = fs.readFileSync(path.join(__dirname, '..', 'user_data.json'), 'utf-8');
             userData = JSON.parse(fileData);
         } catch (error) {}
+		
 		const responseEmbed = new EmbedBuilder(config.embedFormat).setTimestamp().setAuthor({name: `/${this.data.name}`}).addFields({
 			name: `Terms of Service / Privacy Statement`,
 			value: `If you have your direct messages open, then you should have recieved a DM from Catnap explaining the TOS. If not, please enable server members to DM you and re-run this command. Then, you can disable them if you wish.`
 		})
-		const dmEmbed = new EmbedBuilder(config.embedFormat).setTimestamp().setAuthor({name: `/${this.data.name}`}).addFields(
-			{
-				name: `Terms of Service / Privacy Statement`,
-				value: `When using this bot, you agree to follow these terms and any mentioned 3rd party terms. When I refer to 'your data' I mean things that are explained at the end in the corresponding section.
-TOS was last updated on <t:1688951921:D>.`
-			},
-			{
-				name: `Catnap Rules`,
-				value: `1. You will not abuse the bot's features to allow for malicious behavior that harms any community the bot is in.
-2. You agree to let the bot store any data you choose to give it with the understanding that it will not be publicized intentionally without your express permission at any time (this includes things like personal settings usage stats, see below for more info).
-3. You will be nice to the bot and others who interact with it.
-4. You understand that you will be subject to punishment if you break the terms.`
-			},
-			{
-				name: `Your Data`,
-				value: `Your data means these things:
-- Publicly accessable information through Discord APIs and profile viewing
-- Data you send to the bot (any application or "slash" (/) command input may be recorded and saved unless otherwise noted) including chat messages for the AI will be saved.
-				
-Your data is safe within Catnap and is not linked directly to you or your presence on Discord when sent to third parties. However, Catnap relies on some third parties to function.
-- OpenAI - Your data that you provide the bot with in relation to AI features is sent to OpenAI for processing. Their policies can be found at https://openai.com/policies. If you choose, I encourage you to read their terms of service and privacy statement before using AI features of any kind, not just on this bot.
-				
-If another third party is given access to your data within the bot, this section will be updated and you will have to re-agree to the updated terms upon attempting to use the bot.`
-			},
-			{
-				name: `Agreement`,
-				value: `Please read over some or all of these before continuing to use the bot.
-When you are done, use \` /settos \` to agree.`
-			}
-		)
+		
+		const tosFields = Object.entries(config.tos).map(([name, value]) => ({
+            name,
+            value,
+        }));
+
+		const dmEmbed = new EmbedBuilder(config.embedFormat).setTimestamp().addFields(...tosFields);
+
 		await interaction.reply({ embeds: [responseEmbed], ephemeral: userData[interaction.user.id]?.ephemeral ?? true });
 		
 		interaction.user.send({ embeds: [dmEmbed] })
--- index.js
+++ index.js
@@ -4,27 +4,34 @@ const path = require('node:path');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const config = require('./config.json');
 const { loginator } = require('./loginator.js');
+const semver = require('semver')
 
-async function checkLatestVersion() {
+async function checkVersion() {
 	try {
-		const response = await axios.get('https://api.github.com/repos/182exe/catnap/releases/latest');
-		const latestVersion = response.data.tag_name;
-	  
+	  	const response = await axios.get('https://api.github.com/repos/182exe/catnap/releases/latest');
+	  	const releasedVersion = response.data.tag_name;
+		
 	  	const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-	  	const currentVersion = packageJson.version;
-	  
-	  	return [latestVersion !== currentVersion, latestVersion, currentVersion]
+	  	const localVersion = packageJson.version;
+		
+	  	return [releasedVersion, localVersion];
 	} catch (error) {
 	  	loginator(`Error occurred while checking for the latest version: ${error.message}`, `oops`);
 	}
   
 	return false;
 }
-  
-checkLatestVersion()
+checkVersion()
 	.then(result => {
-	  	if (result[0] === true) {
-			loginator(`You may need to update your bot! Version from latest release does not match your version.\nLatest: ${result[1]}\nLocal: ${result[2]}\nIf you're developing a new version for a PR, note that only 182exe handles version control, so don't worry about changing the version in package.json.`, `warn`)
+	  	const releasedVersion = result[0];
+	  	const localVersion = result[1];
+		
+	  	if (releasedVersion !== localVersion && semver.gt(releasedVersion, localVersion)) {
+			loginator(`You may need to update your bot! Version from latest release is newer than your version.\nLatest: ${releasedVersion}\nLocal: ${localVersion}\nIf you're PRing, just note that "management" handles version control, so don't worry about changing the version in package.json.`, `warn`);
+		} else if (releasedVersion !== localVersion && semver.lt(releasedVersion, localVersion)) {
+			loginator(`Happy Developing! (you have an unreleased and newer-than-newest version on your machine)`, `info`);
+		} else if (releasedVersion === localVersion) {
+			loginator(`You're on the newest version of Catnap! (${localVersion})`, `info`);
 		}
 	})
 	.catch(error => {
