d79121bd06652750a97ea0821d25d7e3de327b00
182exe <81325987+182exe@users.noreply.github.com>
2023-12-29 11:55:12 -0700

    fix crash on ai use

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -33,7 +33,7 @@ async function getPreviousMessages(interaction, userId, botId, guildData) {
 			try {
 				referencedMessage = await channel.messages.fetch(msg.reference.messageId);
 			} catch (error) {
-				loginator(error, `oops`)
+				logger.log(error, `oops`)
 			}
 			if (referencedMessage.author.id === userId) {
 				return {
